46e5fc429710fbb70d9d56016f7ab175a2cf079e
devboy <dominic.graefen@gmail.com>
2011-07-06 19:58:38 +0200

    Typo fix.

--- lib/gator/command.rb
+++ lib/gator/command.rb
@@ -1,11 +1,12 @@
 module Gator
   class Command < Thor
+
     def self.register_command(target, name, usage, description, mappings=[], options={})
       target.register self, name, usage, description, options
       target.map mappings => name
     end
 
-    #The following lines fixes a bug in thor see: https://github.com/wycats/thor/pull/150
+    #The following lines fix a bug in thor see: https://github.com/wycats/thor/pull/150
     class << self
       def register(klass, subcommand_name, usage, description, options={})
         if klass <= Thor::Group
