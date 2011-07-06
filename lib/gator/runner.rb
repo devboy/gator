82144b14d73c5d639b6a592a4835c3635a98f665
devboy <dominic.graefen@gmail.com>
2011-07-06 12:15:34 +0200

    Gator::Runner extends Gator::Command now.

--- lib/gator/runner.rb
+++ lib/gator/runner.rb
@@ -3,7 +3,7 @@ require 'thor/group'
 require 'thor/actions'
 
 module Gator
-  class Runner < Thor
+  class Runner < Command
     include Thor::Actions
 
     def self.start
@@ -14,19 +14,6 @@ module Gator
     desc "version", "Show Gator version"
     def version
       say "Gator --pre"
-    end
-
-    #The following lines fixes a bug in thor see: https://github.com/wycats/thor/pull/150
-    class << self
-      def register(klass, subcommand_name, usage, description, options={})
-        if klass <= Thor::Group
-          desc usage, description, options
-          define_method(subcommand_name) { |*args| invoke klass }
-        else
-          desc usage, description, options
-          subcommand subcommand_name, klass
-        end
-      end
     end
 
   end
