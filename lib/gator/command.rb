557856f63413aee251897173a01bfb86b4036e97
devboy <dominic.graefen@gmail.com>
2011-07-06 10:36:54 +0200

    Fixes a bug in thor! See: https://github.com/wycats/thor/pull/150

--- lib/gator/command.rb
+++ lib/gator/command.rb
@@ -4,5 +4,19 @@ module Gator
       target.register self, name, usage, description, options
       target.map mappings => name
     end
+
+    #The following lines fixes a bug in thor see: https://github.com/wycats/thor/pull/150
+    class << self
+      def register(klass, subcommand_name, usage, description, options={})
+        if klass <= Thor::Group
+          desc usage, description, options
+          define_method(subcommand_name) { |*args| invoke klass }
+        else
+          desc usage, description, options
+          subcommand subcommand_name, klass
+        end
+      end
+    end
+
   end
 end
