90e481b6cdecd3fe06114169f035a22bd5f0c828
devboy <dominic.graefen@gmail.com>
2011-07-11 12:09:03 +0200

    Added stub specs.

---
+++ spec/project_spec.rb
@@ -0,0 +1,15 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+
+describe Gator::Project do
+
+  it "should have some tests"
+
+end
+
+describe Gator::Project::Layout do
+
+  it "should have some tests"
+  it "could be the tests from the buildr project"
+
+end
