7ca131d4399d7b0875787c3411d9fdf996e23968
devboy <dominic.graefen@gmail.com>
2011-10-11 18:38:04 +0200

    Added scaffolding command back.

--- lib/gator/plugins/scaffolding.rb
+++ lib/gator/plugins/scaffolding.rb
@@ -1,3 +1,4 @@
+require File.dirname(__FILE__) + '/scaffolding/scaffolding_file_utils'
 require File.dirname(__FILE__) + '/scaffolding/scaffold_command'
 
-Gator::Application << Gator::ScaffoldCommand
+Gator::Application << Gator::Scaffolding::ScaffoldCommand
--- lib/gator/plugins/scaffolding/scaffold_command.rb
+++ lib/gator/plugins/scaffolding/scaffold_command.rb
@@ -3,85 +3,65 @@ require "thor/actions"
 require "fileutils"
 
 class Gator
-  class ScaffoldCommand < Command
-    include Thor::Actions
-    #include Gator::Configuration
+  module Scaffolding
+    class ScaffoldCommand < Command
+      include Thor::Actions
 
-    define :command => "scaffold", :short => "s",
-           :usage => "scaffold TASK", :description => "Set of tasks to manage scaffold templates."
+      define :command => "scaffold", :short => "s",
+             :usage => "scaffold TASK", :description => "Set of tasks to manage scaffold templates."
 
-    def self.source_root
-      Gator::Util.scaffold_template_root
-    end
-
-    desc "scaffold install NAME TEMPLATE_NAME", "Install a scaffold template."
-    def install( dir, template=nil )
-      entries = directory_entries dir
-      template ||= File.expand_path( File.dirname(entries.first) ).split(File::SEPARATOR).last
-      empty_directory template_dir(template)
-      FileUtils.cp_r entries, template_dir(template)
-      create_empty_directory_files template_dir(template)
-    end
-
-    desc "scaffold uninstall TEMPLATE_NAME", "Uninstall a scaffold template."
-    def uninstall( template )
-      FileUtils.rm_r template_dir(template)
-    end
-
-    desc "scaffold new NAME TEMPLATE_NAME", "Create a new scaffold by template."
-    def new( name, template )
-      @scaffold_name = name
-      directory template_dir(template), File.expand_path( name )
-    end
-
-    desc "scaffold wipe", "Delete all scaffold templates."
-    def wipe
-      template_root_entries(true).each { |e| FileUtils.rm_r e }
-    end
-
-    desc "scaffold list [SEARCH]", "Lists scaffold templates."
-    def list(search=nil)
-      entries = template_root_entries
-      entries = entries.select { |e| e.include?(search) } unless search.nil?
-      entries.each {|e| say "  #{e}" }
-      say "No templates found.", :blue if entries.empty?
-    end
-
-    no_tasks {
-
-      def scaffold_name
-        @scaffold_name
+      def self.source_root
+        Gator::Scaffolding::ScaffoldingFileUtils.new.scaffolds_home
       end
 
-      def configuration
-        config
+      desc "scaffold install NAME TEMPLATE_NAME", "Install a scaffold template."
+      def install(dir, name=nil)
+        entries = file_util.directory_entries dir
+        name ||= file_util.last_directory_name dir
+        install_dir = file_util.scaffold_directory(name)
+        empty_directory install_dir
+        FileUtils.cp_r entries, install_dir
+        file_util.create_empty_directory_files install_dir
       end
 
-    }
+      desc "scaffold uninstall TEMPLATE_NAME", "Uninstall a scaffold template."
+      def uninstall(template)
+        FileUtils.rm_r file_util.scaffold_directory(template)
+      end
 
-    private
+      desc "scaffold new NAME TEMPLATE_NAME", "Create a new scaffold by template."
+      def new(name, template)
+        @scaffold_name = name
+        directory file_util.scaffold_directory(template), File.expand_path(name)
+      end
 
-    def create_empty_directory_files( dir )
-      Dir.glob( File.join( dir, "**", "*" ) ).each { |f|
-        create_file File.join(f,".empty_directory"), ".empty_directory" if File.directory?(f) && Dir.entries(f).length == 2
+      desc "scaffold wipe", "Delete all scaffold templates."
+      def wipe
+        file_util.scaffold_entries(true).each { |e| FileUtils.rm_r e }
+      end
+
+      desc "scaffold list [SEARCH]", "Lists scaffold templates."
+      def list(search=nil)
+        entries = file_util.scaffold_entries
+        entries = entries.select { |e| e.include?(search) } unless search.nil?
+        entries.each { |e| say "  #{e}" }
+        say "No templates found.", :blue if entries.empty?
+      end
+
+      no_tasks {
+
+        def scaffold_name
+          @scaffold_name
+        end
+
       }
-    end
 
-    def template_dir( template )
-      File.join( Gator::Util.scaffold_template_root, template )
-    end
+      private
 
-    def directory_entries( dir, join_with_dir=true )
-      entries = Dir.entries(dir)
-      entries.delete(".")
-      entries.delete("..")
-      entries.collect! { |e| File.join(dir, e) } if join_with_dir
-      entries
-    end
+      def file_util
+        @_file_util ||= Gator::Scaffolding::ScaffoldingFileUtils.new
+      end
 
-    def template_root_entries( join_with_dir=false )
-      directory_entries Gator::Util.scaffold_template_root, join_with_dir
     end
-
   end
 end
