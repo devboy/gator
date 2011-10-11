16b2ef87f97827be2ed84c5e085b38e6825f6352
devboy <dominic.graefen@gmail.com>
2011-10-11 15:40:11 +0200

    Refactored configuration specs.

--- spec/core/configuration/configuration_spec.rb
+++ spec/core/configuration/configuration_spec.rb
@@ -1,17 +1,15 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
-
-
-class ConcreteConfiguration < Gator::Configuration
-end
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Gator::Configuration do
 
-  it "should be covered by ActAsConfiguration spec" do
+  before :all do
+    class ConcreteConfiguration < Gator::Configuration
+    end
   end
 
-end
-
-describe Gator::ActAsConfiguration do
+  after :all do
+    remove_const ConcreteConfiguration
+  end
 
   before :each do
     @configuration = ConcreteConfiguration.new
